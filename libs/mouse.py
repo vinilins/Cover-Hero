c9f3a49bc7e3222185cf77f631e589799c1d582d
Vinicius Lins <vinny230cross@gmail.com>
2019-09-03 15:17:10 -0300

    Correção biblioteca mouse.py

--- libs/mouse.py
+++ libs/mouse.py
@@ -1,15 +1,14 @@
-def click_botao_iniciar(mouse_pos, mouse_pressed):
-        if 50+170 > mouse_pos[0] > 50 and 214+31 > mouse_pos[1] > 214 and mouse_pressed[0]==1:
-                return True
-                
-        return False
+# def click_botao_iniciar(mouse_pos, mouse_pressed):
+#         if 50+170 > mouse_pos[0] > 50 and 214+31 > mouse_pos[1] > 214 and mouse_pressed[0]==1:
+#                 return True        
+#         return False
 
-def sobre_botao_iniciar(mouse_pos):
-        if 50+170 > mouse_pos[0] > 50 and 214+31 > mouse_pos[1] > 214:
-                return True
-        return False
+# def sobre_botao_iniciar(mouse_pos):
+#         if 50+170 > mouse_pos[0] > 50 and 214+31 > mouse_pos[1] > 214:
+#                 return True
+#         return False
 
-def sobre_botao_preferencia(mouse_pos):
-        if 46+176 > mouse_pos[0] > 46 and 273+32 > mouse_pos[1] > 273:
-                return True
-        return Falses
+# def sobre_botao_preferencia(mouse_pos):
+#         if 46+176 > mouse_pos[0] > 46 and 273+32 > mouse_pos[1] > 273:
+#                 return True
+#         return False
